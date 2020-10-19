30103422f5929acc938ab89e401fa0bd64cc41b5
Nine-08 <1002838027@qq.com>
2020-10-19 10:27:54 +0800

    曾迪

---
+++ 曾迪BS周作业/js/index.js
@@ -0,0 +1,9 @@
+// var w = document.documentElement.getBoundingClientRect().width;
+// var bili = w / 11.05
+// document.documentElement.style.fontSize = bili + "px";
+
+var wow=new WOW({
+    boxClass: 'mywow',
+    offset:  150  
+})
+wow.init();
